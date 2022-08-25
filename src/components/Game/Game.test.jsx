8295b79f31bf701fa593b42c9ce0bc773d06d894
Alexei <lohanecapho2000@gmail.com>
2022-08-25 13:34:52 +0300

    added snapshot test

--- src/components/Game/Game.test.jsx
+++ src/components/Game/Game.test.jsx
@@ -1,5 +1,6 @@
 import "@testing-library/jest-dom/extend-expect"
 import { render, screen } from "@testing-library/react"
+import renderer from "react-test-renderer"
 import Game from "./Game"
 
 describe("Game test", () => {
@@ -7,5 +8,9 @@ describe("Game test", () => {
     render(<Game />)
     const btn = screen.getAllByRole("button")
     expect(btn.length).toBe(10)
+  })
+  test("Game is render correctly", () => {
+    const domTree = renderer.create(<Game />).toJSON()
+    expect(domTree).toMatchSnapshot()
   })
 })
